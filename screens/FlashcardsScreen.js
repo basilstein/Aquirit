a1de88a5d98f1e61b524c8a0976a2ccda360d6ef
basilstein <pdx.basil@gmail.com>
2020-03-27 20:44:50 -0700

    Roman Sucks

--- screens/FlashcardsScreen.js
+++ screens/FlashcardsScreen.js
@@ -23,7 +23,7 @@ export default function FlashcardsScreen() {
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
 
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
+          <Text style={styles.getStartedText}>Roman Sucks</Text>
 
           <View style={[styles.codeHighlightContainer, styles.FlashcardsScreenFilename]}>
             <MonoText>screens/FlashcardsScreen.js</MonoText>
