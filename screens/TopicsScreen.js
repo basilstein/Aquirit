4ca43e49d0ebfcfc1ecc28e8c62732496e2dddfe
basilstein <pdx.basil@gmail.com>
2020-04-07 17:26:26 -0700

    ...

--- screens/TopicsScreen.js
+++ screens/TopicsScreen.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
 import {RectButton, ScrollView } from 'react-native-gesture-handler';
 
-import Greetings from '../screens/topics/introductions/Greetings.js';
+import Greetings from '../screens/topics/introductions/Greetings';
 
 export default function TopicsScreen() {
   return (
--- screens/topics/introductions/greetings.js
+++ screens/topics/introductions/greetings.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
 import {RectButton, ScrollView } from 'react-native-gesture-handler';
 
 export default function Greetings() {
